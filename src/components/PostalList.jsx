131a6c34eac947a03b7cce92402c705cd7e3d6ef
AJ <abduljaberaj00786@gmail.com>
2025-03-31 11:54:43 +0530

    chore: fix padding

--- src/components/PostalList.jsx
+++ src/components/PostalList.jsx
@@ -2,7 +2,7 @@ import PostalCard from "./PostalCard.jsx";
 
 export default function PostalList({ postalData }) {
   return (
-    <div className="max-h-[60vh] overflow-y-auto space-y-2 border border-gray-300 dark:border-gray-700 rounded-md">
+    <div className="max-h-[60vh] overflow-y-auto space-y-2 p-1 border border-gray-300 dark:border-gray-700 rounded-md">
       {postalData.map((office, index) => (
         <PostalCard key={index} office={office} />
       ))}
